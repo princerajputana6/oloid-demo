818c28a9044dd5b5a8fe5a1afca415cd24599e16
Prince Rajput <prince.kumar@aurigait.com>
2023-08-29 16:05:50 +0530

    testimonial section css added

--- components/Workplace.js
+++ components/Workplace.js
@@ -46,66 +46,75 @@ function Workplace() {
                 </div>
                 <div className='workplace-mobile-cards flex flex-wrap gap-[1.25rem] mt-[3rem]'>
                     <Accordion className="w-full border-none">
-                        <Accordion.Panel className='workplace-card'>
-                            <Accordion.Title style={{border:"1px solid lightgray",marginBottom:"36px",borderRadius:"20px", backgroundImage:"url(https://cdn-gkbkl.nitrocdn.com/EahsqmkNZSarsOQvgGaWKiasFdUDbxev/assets/images/optimized/rev-4a9b36e/www.oloid.ai/wp-content/uploads/2023/07/testimonialimg3.png)", backgroundPosition:"center",backgroundSize:"cever"}}>
-                                Factories and Warehouses
-                                {/* <img src="https://cdn-gkbkl.nitrocdn.com/EahsqmkNZSarsOQvgGaWKiasFdUDbxev/assets/images/optimized/rev-4a9b36e/www.oloid.ai/wp-content/uploads/2023/07/testimonialimg3.png" alt="workplace card" /> */}
-                            </Accordion.Title>
-                            <Accordion.Content className='bg-[#f2f4f9] mt-[-36px]'>
-                                <p>End-to-end safety & security solutions for the deskless worker.</p>
-                                <Testimonial />
-                                <a href="" className='inline-flex items-center px-3 pl-0 py-2 text-[18px] leading-[20px] text-center text-[#2265B0] avenirBlack'>
-                                    Learn more &nbsp;
-                                    <span className="blue-arrow">
-                                        <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.31451 1.18457L12.25 6.12008L7.31451 11.0556" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12.2499 6.12012L1.75 6.12012" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg>
-                                    </span>
-                                </a>
-                            </Accordion.Content>
+                        <Accordion.Panel className='workplace-card' >
+                            <div className='workplace-mobile-card' style={{ background: "url('https://www.oloid.ai/wp-content/uploads/2023/07/testimonialimg3.png')  no-repeat;" }}>
+                                <Accordion.Title >
+                                    Factories and Warehouses
+                                    {/* <img src="https://cdn-gkbkl.nitrocdn.com/EahsqmkNZSarsOQvgGaWKiasFdUDbxev/assets/images/optimized/rev-4a9b36e/www.oloid.ai/wp-content/uploads/2023/07/testimonialimg3.png" alt="workplace card" /> */}
+                                </Accordion.Title>
+                                <Accordion.Content>
+                                    <p>End-to-end safety & security solutions for the deskless worker.</p>
+                                    <Testimonial />
+                                    <a href="" className='inline-flex items-center px-3 pl-0 py-2 text-[18px] leading-[20px] text-center text-[#2265B0] avenirBlack'>
+                                        Learn more &nbsp;
+                                        <span className="blue-arrow">
+                                            <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.31451 1.18457L12.25 6.12008L7.31451 11.0556" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12.2499 6.12012L1.75 6.12012" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg>
+                                        </span>
+                                    </a>
+                                </Accordion.Content>
+                            </div>
+
                         </Accordion.Panel>
                         <Accordion.Panel className='workplace-card'>
-                            <Accordion.Title style={{border:"1px solid lightgray",marginBottom:"36px",borderRadius:"20px", backgroundImage:"url(https://cdn-gkbkl.nitrocdn.com/EahsqmkNZSarsOQvgGaWKiasFdUDbxev/assets/images/optimized/rev-4a9b36e/www.oloid.ai/wp-content/uploads/2023/07/testimonialimg2.png)", backgroundPosition:"center",backgroundSize:"cever"}}>
-                                Enterprise
-                            </Accordion.Title>
-                            <Accordion.Content className='bg-[#f2f4f9] mt-[-36px]'>
-                                <p>End-to-end safety & security solutions for the deskless worker.</p>
-                                <Testimonial />
-                                <a href="" className='inline-flex items-center px-3 pl-0 py-2 text-[18px] leading-[20px] text-center text-[#2265B0] avenirBlack'>
-                                    Learn more &nbsp;
-                                    <span className="blue-arrow">
-                                        <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.31451 1.18457L12.25 6.12008L7.31451 11.0556" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12.2499 6.12012L1.75 6.12012" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg>
-                                    </span>
-                                </a>
-                            </Accordion.Content>
+                        <div className='workplace-mobile-card' style={{ background: "url('https://www.oloid.ai/wp-content/uploads/2023/07/testimonialimg3.png')  no-repeat;" }}>
+                                <Accordion.Title >
+                                    Enterprise
+                                </Accordion.Title>
+                                <Accordion.Content>
+                                    <p>End-to-end safety & security solutions for the deskless worker.</p>
+                                    <Testimonial />
+                                    <a href="" className='inline-flex items-center px-3 pl-0 py-2 text-[18px] leading-[20px] text-center text-[#2265B0] avenirBlack'>
+                                        Learn more &nbsp;
+                                        <span className="blue-arrow">
+                                            <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.31451 1.18457L12.25 6.12008L7.31451 11.0556" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12.2499 6.12012L1.75 6.12012" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg>
+                                        </span>
+                                    </a>
+                                </Accordion.Content>
+                            </div>
+                        </Accordion.Panel>
+                        <Accordion.Panel className='workplace-card' >
+                        <div className='workplace-mobile-card' style={{ background: "url('https://www.oloid.ai/wp-content/uploads/2023/07/testimonialimg3.png')  no-repeat;" }}>
+                                <Accordion.Title >
+                                    Offices
+                                </Accordion.Title>
+                                <Accordion.Content>
+                                    <p>End-to-end safety & security solutions for the deskless worker.</p>
+                                    <Testimonial />
+                                    <a href="" className='inline-flex items-center px-3 pl-0 py-2 text-[18px] leading-[20px] text-center text-[#2265B0] avenirBlack'>
+                                        Learn more &nbsp;
+                                        <span className="blue-arrow">
+                                            <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.31451 1.18457L12.25 6.12008L7.31451 11.0556" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12.2499 6.12012L1.75 6.12012" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg>
+                                        </span>
+                                    </a>
+                                </Accordion.Content>
+                            </div>
                         </Accordion.Panel>
                         <Accordion.Panel className='workplace-card'>
-                            <Accordion.Title style={{border:"1px solid lightgray",marginBottom:"36px",borderRadius:"20px",backgroundImage:"url(https://cdn-gkbkl.nitrocdn.com/EahsqmkNZSarsOQvgGaWKiasFdUDbxev/assets/images/optimized/rev-4a9b36e/www.oloid.ai/wp-content/uploads/2023/07/Rectangle-872-3.png)", backgroundPosition:"center",backgroundSize:"cever"}}>
-                                Offices
-                            </Accordion.Title>
-                            <Accordion.Content className='bg-[#f2f4f9] mt-[-36px] '>
-                                <p>End-to-end safety & security solutions for the deskless worker.</p>
-                                <Testimonial />
-                                <a href="" className='inline-flex items-center px-3 pl-0 py-2 text-[18px] leading-[20px] text-center text-[#2265B0] avenirBlack'>
-                                    Learn more &nbsp;
-                                    <span className="blue-arrow">
-                                        <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.31451 1.18457L12.25 6.12008L7.31451 11.0556" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12.2499 6.12012L1.75 6.12012" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg>
-                                    </span>
-                                </a>
-                            </Accordion.Content>
-                        </Accordion.Panel>
-                        <Accordion.Panel className='workplace-card'>
-                            <Accordion.Title style={{border:"1px solid lightgray",marginBottom:"36px",borderRadius:"20px",backgroundImage:"url(https://cdn-gkbkl.nitrocdn.com/EahsqmkNZSarsOQvgGaWKiasFdUDbxev/assets/images/optimized/rev-4a9b36e/www.oloid.ai/wp-content/uploads/2023/07/testimonialimg1.png)", backgroundPosition:"center",backgroundSize:"cever"}}>
-                                Commercial Real Estate
-                            </Accordion.Title>
-                            <Accordion.Content className='bg-[#f2f4f9] mt-[-36px]'>
-                                <p>End-to-end safety & security solutions for the deskless worker.</p>
-                                <Testimonial />
-                                <a href="" className='inline-flex items-center px-3 pl-0 py-2 text-[18px] leading-[20px] text-center text-[#2265B0] avenirBlack'>
-                                    Learn more &nbsp;
-                                    <span className="blue-arrow">
-                                        <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.31451 1.18457L12.25 6.12008L7.31451 11.0556" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12.2499 6.12012L1.75 6.12012" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg>
-                                    </span>
-                                </a>
-                            </Accordion.Content>
+                        <div className='workplace-mobile-card' style={{ background: "url('https://www.oloid.ai/wp-content/uploads/2023/07/testimonialimg3.png')  no-repeat;" }}>
+                                <Accordion.Title >
+                                    Commercial and Real Estate
+                                </Accordion.Title>
+                                <Accordion.Content>
+                                    <p>End-to-end safety & security solutions for the deskless worker.</p>
+                                    <Testimonial />
+                                    <a href="" className='inline-flex items-center px-3 pl-0 py-2 text-[18px] leading-[20px] text-center text-[#2265B0] avenirBlack'>
+                                        Learn more &nbsp;
+                                        <span className="blue-arrow">
+                                            <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"> <path d="M7.31451 1.18457L12.25 6.12008L7.31451 11.0556" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> <path d="M12.2499 6.12012L1.75 6.12012" stroke="#2265B0" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </svg>
+                                        </span>
+                                    </a>
+                                </Accordion.Content>
+                            </div>
                         </Accordion.Panel>
                     </Accordion>
                 </div>
