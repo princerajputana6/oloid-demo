7bf9c269a09b10415f9b2e6015585cfbc52a17ef
Prince Rajput <prince.kumar@aurigait.com>
2023-08-31 16:36:15 +0530

    .

--- components/Header.js
+++ components/Header.js
@@ -201,7 +201,7 @@ export default function Example() {
         <div className="flex lg:hidden">
             <button
               type="button"
-              className="-m-2.5 bg-[#233751] inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+              className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
               onClick={() => setMobileMenuOpen(true)}
             >
               <span className="sr-only">Open main menu</span>
