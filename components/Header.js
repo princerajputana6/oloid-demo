b3794a1a451c23eac92af5495235c7ccd229886a
Prince Rajput <prince.kumar@aurigait.com>
2023-09-04 11:19:25 +0530

    .

--- components/Header.js
+++ components/Header.js
@@ -212,9 +212,9 @@ export default function Example() {
           </button>
         </div>
       </nav>
-      <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
-        <div className="fixed inset-0 z-10" />
-        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+      <Dialog as="div" className="lg:hidden bg-[#f1f6f9]" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+        <div className="fixed bg-[#f1f6f9] inset-0 z-10" />
+        <Dialog.Panel className="bg-[#f1f6f9] fixed inset-y-0 right-0 z-10 w-full overflow-y-aut px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
 
@@ -230,7 +230,7 @@ export default function Example() {
               <XMarkIcon className="h-6 w-6" aria-hidden="true" />
             </button>
           </div>
-          <div className="mt-6 flow-root">
+          <div className="mt-6 bg-[#fff] p-4  flow-root">
             <div className="-my-6 divide-y divide-gray-500/10">
               <div className="space-y-2 py-6">
                 <Disclosure as="div" className="-mx-3">
@@ -334,10 +334,6 @@ export default function Example() {
                   )}
                 </Disclosure>
 
-              </div>
-              <div className="py-6">
-
-                <button class="bg-[#2265B0] -mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-[#233650] hover:bg-gray-50">Contact Us</button>
               </div>
             </div>
           </div>
